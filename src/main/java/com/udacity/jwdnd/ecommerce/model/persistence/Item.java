2b8ee1b95c4d7ab4d0c088a426c3933a686398fd
Agoston PAJZS <agostonp@gmail.com>
2020-09-18 10:49:12 +0200

    Replace custom equals() and hashcode() with lombok annotation

--- src/main/java/com/udacity/jwdnd/ecommerce/model/persistence/Item.java
+++ src/main/java/com/udacity/jwdnd/ecommerce/model/persistence/Item.java
@@ -12,6 +12,7 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,7 +20,7 @@ import lombok.ToString;
 
 @Entity
 @Table(name = "item")
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString @EqualsAndHashCode
 public class Item {
 
 	@Id
@@ -38,29 +39,4 @@ public class Item {
 	@Column(nullable = false)
 	@JsonProperty
 	private String description;
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Item other = (Item) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}	
 }
